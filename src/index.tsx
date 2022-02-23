ff43219adf2fe20b32b92e44d89739ceb99d9788
BauTancredi <bautistatancredi@hotmail.com>
2022-02-23 09:32:21 -0300

    Display MapView from mapbox

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
 
 import { MapsApp } from './MapsApp';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmF1dGFuY3JlZGkiLCJhIjoiY2t6emoyNTVmMGFyMTNrcXhydHk4aTd4eiJ9.eKwtcnskmvaZfL1_h00ECA';
 
 if (!navigator.geolocation) {
   alert('Your browser has no geolocation option');
